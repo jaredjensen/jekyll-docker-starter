8c0b60be2e6437a93565e41a01e2d29fab53a697
Jared Jensen <jared.jensen@forgerock.com>
2019-05-22 10:02:14 -0700

    Cleanup

--- src/assets/site/js/modules/video-feed.js
+++ src/assets/site/js/modules/video-feed.js
@@ -1,19 +1,21 @@
 ﻿(function ($) {
 
-	$('.video-feed').each(buildFeed);
+  var apiKey = '<your key>';
 
-	function buildFeed() {
-		var self = $(this);
-		var numVideos = self.data('numVideos') || 3;
-		var numCols = 12 / numVideos;
-		var url = 'https://www.googleapis.com/youtube/v3/search?part=id&forMine=true&order=date&type=video&key=AIzaSyA6-lj3O_SU2zKV4GmM_aZuNbCfvNqtyYM&maxResults=' + numVideos;
-		$.getJSON(url, function (data) {
-			var html = [];
-			for (var i = 0; i < data.items.length; i++) {
-				html.push('<div class="col-md-' + numCols + '><div class="video-container"><iframe src="http://www.youtube.com/embed/' + data.items[i].id + '" frameborder="0" allowfullscreen></iframe></div></div>');
-			}
-			self.html(html);
-		});
-	}
+  $('.video-feed').each(buildFeed);
+
+  function buildFeed() {
+    var self = $(this);
+    var numVideos = self.data('numVideos') || 3;
+    var numCols = 12 / numVideos;
+    var url = `https://www.googleapis.com/youtube/v3/search?part=id&forMine=true&order=date&type=video&key=${apiKey}&maxResults=${numVideos}`;
+    $.getJSON(url, function (data) {
+      var html = [];
+      for (var i = 0; i < data.items.length; i++) {
+        html.push(`<div class="col-md-${numCols}><div class="video-container"><iframe src="http://www.youtube.com/embed/' + data.items[i].id + '" frameborder="0" allowfullscreen></iframe></div></div>`);
+      }
+      self.html(html);
+    });
+  }
 
 }(window.jQuery));
